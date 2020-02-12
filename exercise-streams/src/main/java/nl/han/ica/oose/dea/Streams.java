62b35f55689950bcbf1c9e193df2907975b65daf
Aaron van den Berg <a.vdberg98@gmail.com>
2020-02-12 19:15:06 +0100

    Added Java Streams exercises

---
+++ exercise-streams/src/main/java/nl/han/ica/oose/dea/Streams.java
@@ -0,0 +1,37 @@
+package nl.han.ica.oose.dea;
+import nl.han.ica.oose.dea.helpers.Product;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+class Streams {
+
+    List<String> filterStringsLongerThanThreeCharacters(List<String> input) {
+        return input.stream().filter(string -> string.length() < 3).collect(Collectors.toList());
+    }
+
+    List<String> filterStringsThanContainOnlyNumerals(List<String> input) {
+        return input.stream().filter(string -> string.matches("[0-9]+")).collect(Collectors.toList());
+    }
+
+    String findShortestString(List<String> input) {
+        // implement
+        return input.stream().reduce(String.valueOf(0), (i, string) -> {
+            if (i.length() < string.length())
+                string = i;
+
+            return string;
+        });
+    }
+
+    String createAFullSentenceFromTheList(List<String> input) {
+        return input.stream().collect(Collectors.joining(" "));
+    }
+
+    int calculateTotalCostOfAllProducts(List<Product> products) {
+        // implement
+
+        return 0;
+    }
+}
