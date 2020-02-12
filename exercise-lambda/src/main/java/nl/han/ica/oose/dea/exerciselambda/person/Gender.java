659f1c5dc851eb093bafe6474167913c2f30bfc4
Aaron van den Berg <a.vdberg98@gmail.com>
2020-02-12 19:10:53 +0100

    Added Lambda Expression exercise

---
+++ exercise-lambda/src/main/java/nl/han/ica/oose/dea/exerciselambda/person/Gender.java
@@ -0,0 +1,12 @@
+package nl.han.ica.oose.dea.exerciselambda.person;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+public enum Gender {
+    MALE,
+    FEMALE;
+
+
+}
