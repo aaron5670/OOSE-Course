83a6c02f243d1263a465b3c3a5d6ca5a832c6b3e
Aaron van den Berg <a.vdberg98@gmail.com>
2020-03-11 10:53:22 +0100

    Removed unused import

--- SOLID-Principle-Movie/src/test/java/ooad/aaron/MovieTest.java
+++ SOLID-Principle-Movie/src/test/java/ooad/aaron/MovieTest.java
@@ -1,7 +1,6 @@
 package ooad.aaron;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
